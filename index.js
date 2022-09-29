0f6d45133be508c984acd7ba2a269c39a24d985b
jerednav <80506833+jerednav@users.noreply.github.com>
2022-09-29 08:24:43 -0700

    added book constructor, add some starting books

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 import './style.css';
 
+//Create Library array
+let myLibrary = [];
+
+//Book constructor
+class Book {
+  constructor(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+  }
+  getInfo(){
+    return `${title} by ${author}, ${pages}pages, ${haveRead === true ? "read" : "not read"} `
+  }
+}
+
+// Adds some starting books to work with
+myLibrary.push(new Book('The Hobbit', 'J. R. R. Tolken', 200, true));
+myLibrary.push(new Book('Harry Potter', 'J. K. Rowling', 200, true));
+myLibrary.push(new Book('Models', 'Mark Mason', 200, true));
+myLibrary.push(new Book('Greenlights', 'Matthew McConaughey', 200, false));
+myLibrary.push(new Book('A Promised Land', 'Barack Obama', 200, false));
+myLibrary.push(new Book('The Food Lab', 'J. Kenji Lopez-Alt', 200, false));
+
